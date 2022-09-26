833a1f1af2700b9688a77cc4d44cb4b83de48699
Maximilian Dietz <maxidietz96@gmail.com>
2022-09-26 10:55:34 +0200

    AlvinsSchatzsuche

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
+import cv2
+import numpy as np
+from tasks import AlvinsSchatzsuche
+
+
 def main():
-    print("hello world")
+    oAlvinsSchatzsuche = AlvinsSchatzsuche()
+    #oAlvinsSchatzsuche.calculate_center()
+    oAlvinsSchatzsuche.recognize_ropes_of_image()
+
+
+    # ranges = {
+    #     "red": ([0, 0, 128], [60, 60, 255]),  # B G R
+    #     "yellow": ([30, 100, 140], [70, 200, 220]),  # B G R
+    #     "blue": ([90, 0, 0], [255, 50, 50]),  # B G R
+    # }
+    # image = cv2.imread('assets/alvins-schatzsuche-1.png')
+    
+    # for key in ranges.keys():
+    #     lowerRange = np.array(ranges[key][0], dtype="uint8")
+    #     upperRange = np.array(ranges[key][1], dtype="uint8")
+ 
+ 
+    #     mask = cv2.inRange(image, lowerRange, upperRange)
+    #     output = cv2.bitwise_and(image, image, mask=mask)
+ 
+    #     # show the images
+    #     cv2.imshow(key, output)
+    #     cv2.waitKey(0)
+
+    # cv2.destroyAllWindows()
+
+    
+    # cv2.imshow('Original Image', image)
+    # cv2.waitKey(0)
 
 
 if __name__ == "__main__":
---
+++ tasks.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+
+
+class Task():
+
+    def __init__(self):
+        pass
+
+    def calculate_center_of_image(self):
+        image = cv2.imread('assets/alvins-schatzsuche-1.png')
+        nHeight = np.size(image, 0)
+        nWidth = np.size(image, 1)
+        nX1 = (int)(nWidth / 2)
+        nX2 = (int)(nWidth / 2)
+        nY1 = 0
+        nY2 = nHeight
+        nLineThickness = 3
+        cv2.line(image, (nX1, nY1), (nX2, nY2), (0, 255, 0), nLineThickness)
+        # show the images
+        cv2.imshow("", image)
+        cv2.waitKey(0)
+
+
+class AlvinsSchatzsuche(Task):
+
+    def __init__(self):
+        super(Task, self).__init__()
+
+    def ropes_to_array(self, oRangeRopes):
+        #unendliche Zeilen, 5 Spalten
+        aResult = [0][0, 0, 0, 0, 0]
+
+        pass
+
+    def recognize_ropes_of_image(self):
+        oImage = cv2.imread('assets/alvins-schatzsuche-1.png')
+
+        def helper_recognize(aLower, aUpper):
+            oMask = cv2.inRange(oImage, np.array(aLower, dtype="uint8"),
+                                np.array(aUpper, dtype="uint8"))
+            return cv2.bitwise_and(oImage, oImage, mask=oMask)
+
+        oRanges = {
+            "red": ([0, 0, 128], [60, 60, 255]),  # B G R
+            "yellow": ([30, 100, 140], [70, 200, 220]),  # B G R
+            "blue": ([90, 0, 0], [255, 50, 50]),  # B G R
+        }
+        oResult = {
+            "oImageRed":
+            helper_recognize(oRanges["red"][0], oRanges["red"][1]),
+            "oImageBlue":
+            helper_recognize(oRanges["blue"][0], oRanges["blue"][1]),
+            "oImageYellow":
+            helper_recognize(oRanges["yellow"][0], oRanges["yellow"][1])
+        }
+        # show the images
+        cv2.imshow("fef", oResult["oImageRed"])
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return oResult
+
